46b8508defb1289f69a7e7d1c19c1b64678f353c
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-08-24 21:10:03 +0300

    Rename functions to avoid name clashes

--- zsh-edit.plugin.zsh
+++ zsh-edit.plugin.zsh
@@ -10,7 +10,7 @@ zsh-edit() {
   local dir=${${(%):-%x}:A:h}
   local fdir=$dir/functions
   typeset -gU FPATH fpath=( $dir $fpath )
-  autoload -Uz $fdir/bind $fdir/_*~*.zwc(N)
+  autoload -Uz $fdir/bind $fdir/[._]edit.*~*.zwc(DN)
 
   bindkey -M emacs '^U'  backward-kill-line
   bindkey -M emacs '^[e' redo
@@ -21,8 +21,8 @@ zsh-edit() {
   .end-of-buffer() {
     CURSOR=$#BUFFER
   }
-  zle -N beginning-of-buffer .beginning-of-buffer
-  zle -N end-of-buffer       .end-of-buffer
+  zle -N {,.}beginning-of-buffer
+  zle -N {,.}end-of-buffer
   bindkey -M emacs '^[<' beginning-of-buffer
   bindkey -M emacs '^[>' end-of-buffer
   bindkey -M menuselect '^[<' beginning-of-history
@@ -36,7 +36,7 @@ zsh-edit() {
       '^[^B' backward-shell-word \
       '^[f'  forward-subword \
       '^[^F' forward-shell-word; do
-    zle -N "$v" _edit_subword
+    zle -N "$v" .edit.subword
     bindkey -M emacs "$k" "$v"
     ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=( "$v" )
   done
@@ -46,24 +46,23 @@ zsh-edit() {
       '^W'   backward-kill-shell-word \
       '^[d'  kill-subword \
       '^[^D' kill-shell-word; do
-    zle -N "$v" _edit_subword
+    zle -N "$v" .edit.subword
     bindkey -M emacs "$k" "$v"
   done
 
   local w
   for w in yank yank-pop reverse-yank-pop vi-put-before vi-put-after; do
-    zle -N $w _visual_yank
+    zle -N $w .edit.visual-yank
   done
   bindkey -M emacs '^[Y' reverse-yank-pop
 
-  zle -N dirstack-minus _dirstack
-  zle -N dirstack-plus  _dirstack
+  zle -N dirstack-minus .edit.dirstack
+  zle -N dirstack-plus  .edit.dirstack
   bindkey -M emacs '^[_' dirstack-minus
   bindkey -M emacs '^[+' dirstack-plus
   bindkey -M menuselect -s '^[_' '^G^_^[_'
   bindkey -M menuselect -s '^[+' '^G^_^[+'
 
-  zle -N _pushd
   bind -M emacs '^[-' 'pushd -1'  # Go backward one dir.
   bind -M emacs '^[=' 'pushd +0'  # Go forward one dir.
   bindkey -M menuselect '^[-' menu-complete
