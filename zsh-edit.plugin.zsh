36f5a9ba8267791ac73ff2ecc0b1f1f136cfe4cf
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-09-18 22:11:41 +0300

    Resolve symlinks in plugin dir

--- zsh-edit.plugin.zsh
+++ zsh-edit.plugin.zsh
@@ -7,7 +7,7 @@ zsh-edit() {
   typeset -gHa _edit_opts=( extendedglob NO_listbeep NO_shortloops warncreateglobal )
   setopt $_edit_opts
 
-  local dir=${${(%):-%x}:A:h}
+  local dir=${${(%):-%x}:P:h}
   local fdir=$dir/functions
   typeset -gU FPATH fpath=( $dir $fpath )
   autoload -Uz $fdir/bind $fdir/[._]edit.*~*.zwc(DN)
