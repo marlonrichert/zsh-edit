4a8fa599792b6d52eadbb3921880a40872013d28
Marlon Richert <marlonrichert@users.noreply.github.com>
2022-05-05 16:38:54 +0300

    Improve "insert word" cycling logic

--- zsh-edit.plugin.zsh
+++ zsh-edit.plugin.zsh
@@ -34,39 +34,39 @@ zsh-edit() {
     zle -N "$widget" .edit.insert-word
   done
 
-  [[ -v ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS ]] ||
-    typeset -gHa ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=()
-  ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(
-      backward-subword backward-shell-word forward-subword forward-shell-word
-  )
-
   .edit.bind() {
-    local widget=$1 emacs=$2
-    shift 2
-    bindkey -M emacs "$emacs" "$widget"
+    local widget=$1
+    shift
+    bindkey -M emacs "$1" "$widget"
     bindkey "${@:^^widget}"
     widget=.$widget
     bindkey -M menuselect "${@:^^widget}"
   }
 
+  bindkey '^?'    backward-delete-char
+  bindkey '^[[3~' delete-char
   .edit.bind backward-subword         '^[b'   '^[^['{\[,O}D   '^['{'[1;',\[,O}{3,5}D
   .edit.bind backward-shell-word      '^[^B'  '^[^['{\[,O}A   '^['{'[1;',\[,O}{3,5}A
   .edit.bind forward-subword          '^[f'   '^[^['{\[,O}C   '^['{'[1;',\[,O}{3,5}C
   .edit.bind forward-shell-word       '^[^F'  '^[^['{\[,O}B   '^['{'[1;',\[,O}{3,5}B
-  bindkey '^?'    backward-delete-char
   .edit.bind backward-kill-subword    '^H'    '^H'  '^[^?'  '^[[27;'{3,5}';8~'
   .edit.bind backward-kill-shell-word '^W'    '^[^H'        '^[[27;'{6,7}';8~'
-  bindkey '^[[3~' delete-char
   .edit.bind kill-subword             '^[d'   '^[(' '^[^[[3~'   '^[[3;'{3,5}\~
   .edit.bind kill-shell-word          '^[^D'        '^[^[[3;5~' '^[[3;'{6,7}\~
-  .edit.bind beginning-of-line        '^A'    '^['{\[,O}H
-  .edit.bind end-of-line              '^E'    '^['{\[,O}F
   .edit.bind beginning-of-buffer      '^[<'   '^['{'[1;',\[,O}5H  '^[[5~'
   .edit.bind end-of-buffer            '^[>'   '^['{'[1;',\[,O}5F  '^[[6~'
-  .edit.bind backward-kill-line       '^U'    '^['{'[1;',\[,O}2H  '^['{'[1;',\[,O}2D
-  .edit.bind kill-line                '^K'    '^['{'[1;',\[,O}2F  '^['{'[1;',\[,O}2C
+  .edit.bind beginning-of-line       '^A'    '^['{\[,O}H
+  .edit.bind end-of-line             '^E'    '^['{\[,O}F
+  .edit.bind backward-kill-line      '^U'    '^['{'[1;',\[,O}2H  '^['{'[1;',\[,O}2D
+  .edit.bind kill-line               '^K'    '^['{'[1;',\[,O}2F  '^['{'[1;',\[,O}2C
 
   unfunction .edit.bind
+
+  [[ -v ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS ]] ||
+    typeset -gHa ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=()
+  ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(
+      end-of-line forward-{shell-,sub}word
+  )
 
   bindkey -M emacs  '^[/' redo  '^[Y' reverse-yank-pop
   bindkey -M emacs  '^[.' insert-last-word  '^[,' insert-first-word \
