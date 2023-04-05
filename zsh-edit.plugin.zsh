bbe9e954b5bb2c510f80a9a5c88ba4f8f098e7c5
Marlon Richert <marlonrichert@users.noreply.github.com>
2023-04-05 09:58:53 +0300

    Handle widgets getting wrapped by another plugin

--- zsh-edit.plugin.zsh
+++ zsh-edit.plugin.zsh
@@ -5,7 +5,8 @@ setopt autopushd pushdminus
   local dir=${${(%):-%x}:P:h}
   local fdir=$dir/functions
   typeset -gU FPATH fpath=( $dir $fpath )
-  builtin autoload -rUz $fdir/{bind,zsh-edit} $fdir/[._]edit.*~*.zwc(DN)
+  autoload -Uz $fdir/{bind,zsh-edit} $fdir/[._]edit.*~*.zwc(DN)
+
   {
     zsh-edit "$@"
   } always {
