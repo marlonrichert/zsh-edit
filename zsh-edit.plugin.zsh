68e831ecdf34e43f076ead416f0bb8a3e431056d
Marlon Richert <marlonrichert@users.noreply.github.com>
2023-05-02 17:47:30 +0300

    Fix shell options
    Fixes #17.

--- zsh-edit.plugin.zsh
+++ zsh-edit.plugin.zsh
@@ -1,7 +1,9 @@
 #!/bin/zsh
+typeset -gHa _edit_opts=( extendedglob NO_listbeep NO_shortloops warncreateglobal )
 setopt autopushd pushdminus
 
 () {
+  setopt $_edit_opts
   local dir=${${(%):-%x}:P:h}
   local fdir=$dir/functions
   typeset -gU FPATH fpath=( $dir $fpath )
