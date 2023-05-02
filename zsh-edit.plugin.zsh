c00c9e53f83c8e1aa5ac4b7d3c7970d8881afe3b
Marlon Richert <marlonrichert@users.noreply.github.com>
2023-05-02 19:40:18 +0300

    Plug shell options leak

--- zsh-edit.plugin.zsh
+++ zsh-edit.plugin.zsh
@@ -1,9 +1,8 @@
 #!/bin/zsh
-typeset -gHa _edit_opts=( extendedglob NO_listbeep NO_shortloops warncreateglobal )
-setopt autopushd pushdminus
-
 () {
-  setopt $_edit_opts
+  typeset -gHa _edit_opts=( extendedglob NO_listbeep NO_shortloops warncreateglobal )
+  emulate -L zsh; setopt $_edit_opts
+
   local dir=${${(%):-%x}:P:h}
   local fdir=$dir/functions
   typeset -gU FPATH fpath=( $dir $fpath )
