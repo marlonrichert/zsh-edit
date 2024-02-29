45a53cf2858ebd9aa36e06913ed354897b89e1df
Marlon Richert <marlonrichert@users.noreply.github.com>
2024-02-29 15:43:27 +0200

    Fix problem with history read from file

--- zsh-edit.plugin.zsh
+++ zsh-edit.plugin.zsh
@@ -1,10 +1,17 @@
 #!/bin/zsh
+
+# Ensure history from file is shell-parsed into words, not split on blanks.
+setopt histlexwords
+
+typeset -ga _edit_opts=( extendedglob NO_listbeep NO_shortloops warncreateglobal )
+
 () {
-  typeset -gHa _edit_opts=( extendedglob NO_listbeep NO_shortloops warncreateglobal )
-  emulate -L zsh; setopt $_edit_opts
+  emulate -L zsh
+  setopt $_edit_opts
 
   local dir=${${(%):-%x}:P:h}
   local fdir=$dir/functions
+
   typeset -gU FPATH fpath=( $dir $fpath )
   autoload -Uz $fdir/{bind,zsh-edit} $fdir/[._]edit.*~*.zwc(DN)
 
